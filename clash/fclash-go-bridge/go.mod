7b096ca7c7669134a93dbf75fc55cf63c7e2eda3
shipinbaoku <3130100@qq.com>
2024-03-26 18:10:24 +0800

    rm all

--- clash/fclash-go-bridge/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/kingtous/fclash-go-bridge
-
-go 1.19
--- lib/http/options.dart
+++
@@ -1,7 +0,0 @@
-
-
-class HttpOptions {
-  // static const String v2boardapi = '';
-  // static const String websiteurl = '';
-  static const String cfgurl = '';
-}
